602f76b2f75328aaa894eef43091b31549001c9c
vsanjuan <salvador.sanjuan@gmail.com>
2016-12-26 22:18:32 +0100

    Revert "initial cost tool version"
    This reverts commit a6afc9a95a8cca861e30e3d6bb45bde982d495c2.
    
    Conflicts:
    	cost_model.py
    	cost_view.xml

--- __openerp__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-{
-	'name': 'Cost calculation tool',
-	'description': 'Calculate your cost from the BOM',
-	'author': 'Salvador Sanjuan',
-	#'depends': [],
-	'data': ['cost_view.xml'],
-}
