4ca60f5c0e96c76fd914712bad598de09bf7ce0f
vsanjuan <salvador.sanjuan@gmail.com>
2016-12-26 23:05:53 +0100

    Back to start

---
+++ __openerp__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+{
+	'name': 'Cost calculation tool',
+	'description': 'Calculate your cost from the BOM',
+	'author': 'Salvador Sanjuan',
+	#'depends': [],
+	'data': ['cost_view.xml'],
+}
